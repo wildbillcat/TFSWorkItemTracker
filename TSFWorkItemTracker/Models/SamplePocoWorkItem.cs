607bb2880ac4d5d07223243e44e7066eebb4baee
Patrick McMorran <patrick.mcmorran@aig.com>
2015-01-30 15:26:04 -0500

    test

---
+++ TSFWorkItemTracker/Models/SamplePocoWorkItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TSFWorkItemTracker.Models
+{
+    public class SamplePocoWorkItem
+    {
+        int Id;
+        , [Team Project], [State], [Request Type], [Urgency], [Request By], [Request Date], [Change Date], [Team Assignment], [Title]
+        
+        public SamplePocoWorkItem()
+        {
+
+        }
+    }
+}
