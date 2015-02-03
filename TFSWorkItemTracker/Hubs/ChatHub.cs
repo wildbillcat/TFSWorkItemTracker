3de0e4496d09a4d381049ab33ff064d2858e3065
Patrick McMorran <kingrolloinc@sbcglobal.net>
2015-02-02 22:19:20 -0500

    Alot of updates to angular, almost figured out toggling!

--- TFSWorkItemTracker/Hubs/ChatHub.cs
+++ TFSWorkItemTracker/Hubs/ChatHub.cs
@@ -22,7 +22,7 @@ namespace TFSWorkItemTracker.Hubs
         //This string maintains the state of the timer log since application start. Just a Proof of concept to later be replaced by workitems.
         static List<string> TimerLog = new List<string>();
         //Stores a list of WorkItems found by the Query
-        static Dictionary<string, List<WorkItem>> TFSWorkItems = new Dictionary<string, List<WorkItem>>();
+        //*******************static Dictionary<string, List<WorkItem>> TFSWorkItems = new Dictionary<string, List<WorkItem>>();
         static Dictionary<string, Dictionary<int, PocoWorkItem>> TFSPocoWorkItems = new Dictionary<string, Dictionary<int, PocoWorkItem>>();
         
         //Stores a list of URIs to access the Work items in TFS
@@ -40,6 +40,10 @@ namespace TFSWorkItemTracker.Hubs
         
         static Object TimerEventLock = new Object();
 
+        static Object ToggleLock = new Object();
+
+        static Dictionary<string, PocoWorkItem> ToggledWorkItems = new Dictionary<string, PocoWorkItem>();
+
         public ChatHub() : base()
         {
             if (TFSPoll == null)
@@ -50,7 +54,7 @@ namespace TFSWorkItemTracker.Hubs
             }
 
             //Ensure Project Collections are fine
-            if (!((TfsProjectCollections.Count() == ProjectCollectionUris.Length) && (TFSServerQuerys.Count() == ProjectCollectionUris.Length) && (TFSWorkItems.Count() == ProjectCollectionUris.Length) && (TFSPocoWorkItems.Count == ProjectCollectionUris.Length)))
+            if (!((TfsProjectCollections.Count() == ProjectCollectionUris.Length) && (TFSServerQuerys.Count() == ProjectCollectionUris.Length) /*&& (TFSWorkItems.Count() == ProjectCollectionUris.Length)*/ && (TFSPocoWorkItems.Count == ProjectCollectionUris.Length)))
             {
                 //This builds a list of project collections for querying later.
                 foreach (string Location in ProjectCollectionUris)
@@ -60,10 +64,10 @@ namespace TFSWorkItemTracker.Hubs
                     {
                         TfsProjectCollections.Add(Team.Name, Team);
                     }
-                    if (!TFSWorkItems.ContainsKey(Team.Name))
+                    /*if (!TFSWorkItems.ContainsKey(Team.Name))
                     {
                         TFSWorkItems.Add(Team.Name, new List<WorkItem>());
-                    }
+                    }*/
                     if (!TFSServerQuerys.ContainsKey(Team.Name))
                     {
                         TFSServerQuerys.Add(Team.Name, new Query((WorkItemStore)Team.GetService(typeof(WorkItemStore)), TFSServerQuery));
@@ -92,10 +96,9 @@ namespace TFSWorkItemTracker.Hubs
         }
 
         static private void OnTimedEvent(Object source, ElapsedEventArgs e)
-        {
-            GlobalHost.ConnectionManager.GetHubContext<ChatHub>().Clients.All.newMessage(string.Concat("The Elapsed event was raised at {0}", e.SignalTime));            
+        {          
             //This holds the results from the queries. Blocking collection allows for queries to be handled in multiple threads
-            Dictionary<string, List<WorkItem>> FreshTFSWorkItems = new Dictionary<string, List<WorkItem>>();
+            //******Dictionary<string, List<WorkItem>> FreshTFSWorkItems = new Dictionary<string, List<WorkItem>>();
             //Start all of the queries against the server (Async)
             Dictionary<string, ICancelableAsyncResult> CallBacks = new Dictionary<string, ICancelableAsyncResult>();
 
@@ -103,7 +106,7 @@ namespace TFSWorkItemTracker.Hubs
             foreach (string ProjectName in TFSServerQuerys.Keys)
             {
                 CallBacks.Add(ProjectName, TFSServerQuerys[ProjectName].BeginQuery());
-                FreshTFSWorkItems.Add(ProjectName, new List<WorkItem>());
+                //**************FreshTFSWorkItems.Add(ProjectName, new List<WorkItem>());
             }
             //Read Only Query Section
             Parallel.ForEach(CallBacks.Keys, ProjName =>
@@ -129,32 +132,61 @@ namespace TFSWorkItemTracker.Hubs
                     {
                         //Query has returned, lets find any new results
                         WorkItemCollection nextresults = TFSServerQuerys[ProjName].EndQuery(CallBacks[ProjName]);
-                        List<WorkItem> Deletion = new List<WorkItem>();
+                        List<int> Deletion = new List<int>();
 
                         //Delete old Work Items Loop 
                         //Builds a list of Items to delete. For each loops do not reevaluate their index, so removing in the loop could cause issues.
                         foreach (PocoWorkItem DeletionCandidate in TFSPocoWorkItems[ProjName].Values)
                         {
-                            if (!FreshTFSWorkItems[ProjName].Exists(P => P.Id == DeletionCandidate.Id))
+                            int ItemIndex = nextresults.IndexOf(DeletionCandidate.Id);
+                            //if the Known Work Item does not exist in the new query results (IndexOf returns -1 if workitem id doesn't exist in query)
+                            if (ItemIndex==-1)
                             {
-                                //Remove from client side
-                                TFSPocoWorkItems[ProjName].Remove(DeletionCandidate.Id);
+                                //Mark the workitem for deletion, Remove from client side
+                                Deletion.Add(DeletionCandidate.Id);
                                 GlobalHost.ConnectionManager.GetHubContext<ChatHub>().Clients.All.removeWorkItem(TFSPocoWorkItems[ProjName][DeletionCandidate.Id]);
                             }
+                            else
+                            {
+                                WorkItem UpdatedWI = nextresults[ItemIndex];
+                                //The work item is known, update any dynamic fields
+                                if (!(UpdatedWI.State == DeletionCandidate.State && UpdatedWI.Project.Name.Equals(DeletionCandidate.Project) && UpdatedWI.ChangedBy.Equals(DeletionCandidate.ChangedBy) && UpdatedWI.Title.Equals(DeletionCandidate.Title)))
+                                {
+                                    DeletionCandidate.State = UpdatedWI.State;
+                                    DeletionCandidate.Project = UpdatedWI.Project.Name;
+                                    DeletionCandidate.ChangedDate = UpdatedWI.ChangedDate;
+                                    DeletionCandidate.ChangedBy = UpdatedWI.ChangedBy;
+                                    DeletionCandidate.Title = UpdatedWI.Title;
+                                    GlobalHost.ConnectionManager.GetHubContext<ChatHub>().Clients.All.updateWorkItem(DeletionCandidate);
+                                    lock (ToggleLock)
+                                    {
+                                        if (ToggledWorkItems.ContainsKey(DeletionCandidate.JsonId))
+                                        {
+                                            GlobalHost.ConnectionManager.GetHubContext<ChatHub>().Clients.All.toggleWorkItem(DeletionCandidate.JsonId);
+                                        }
+                                    }
+                                }
+                            }
                         }
+
+                        //Deletion Loop
+                        foreach (int RemovalKey in Deletion)
+                        {
+                            TFSPocoWorkItems[ProjName].Remove(RemovalKey);
+                            
+                        }
+
                         //Add new Work Items Loop.
                         foreach (WorkItem result in nextresults)
                         {
                             //If the WorkItem doesnt exist in the current Collection, add it and update clients.
-                            if (!TFSWorkItems[ProjName].Contains(result))
+                            if (!TFSPocoWorkItems[ProjName].ContainsKey(result.Id))
                             {
                                 //Method for updating clients will have to be added.
                                 TFSPocoWorkItems[ProjName].Add(result.Id, new PocoWorkItem(result, ProjName));
                                 GlobalHost.ConnectionManager.GetHubContext<ChatHub>().Clients.All.addWorkItem(TFSPocoWorkItems[ProjName][result.Id]);
                             }
                         }
-                        //Now that Clients have been Diff'd replace the WorkItemList
-                        TFSWorkItems[ProjName] = FreshTFSWorkItems[ProjName];
                     }
                 });
             }
@@ -166,6 +198,24 @@ namespace TFSWorkItemTracker.Hubs
             Clients.All.newMessage(string.Concat(Context.User.Identity.Name, ":", message));
         }
 
+        public void Toggle(PocoWorkItem ToggleItem)
+        {
+            lock (ToggleLock)
+            {
+                //exists - Thus untoggle
+                if (ToggledWorkItems.ContainsKey(ToggleItem.JsonId))
+                {
+                    ToggledWorkItems.Remove(ToggleItem.JsonId);
+                    Clients.All.toggleWorkItem(ToggleItem);
+                }
+                else
+                {
+                    ToggledWorkItems.Add(ToggleItem.JsonId, ToggleItem);
+                    Clients.All.toggleWorkItem(ToggleItem);
+                }
+            }
+        }
+
         public override System.Threading.Tasks.Task OnConnected()
         {
             foreach (string header in QueryHeaders)
@@ -175,9 +225,19 @@ namespace TFSWorkItemTracker.Hubs
             lock (TimerEventLock)
             {
                 //Add a lock to prevent clients joining on query
-                foreach (string Log in TimerLog)
+                foreach (Dictionary<int, PocoWorkItem> WIDictionary in TFSPocoWorkItems.Values)
                 {
-                    Clients.Client(Context.ConnectionId).addNewWorkItemToPage(Log);
+                    foreach (PocoWorkItem WI in WIDictionary.Values)
+                    {
+                        Clients.Client(Context.ConnectionId).addWorkItem(WI);
+                    }
+                }
+            }
+            lock (ToggleLock)
+            {
+                foreach (PocoWorkItem TWorkItem in ToggledWorkItems.Values)
+                {
+                    Clients.Client(Context.ConnectionId).toggleWorkItem(TWorkItem);
                 }
             }
             return base.OnConnected();
--- TFSWorkItemTracker/Models/PocoWorkItem.cs
+++ TFSWorkItemTracker/Models/PocoWorkItem.cs
@@ -9,9 +9,11 @@ namespace TFSWorkItemTracker.Models
     public class PocoWorkItem
     {
         public int Id;
+        public string JsonId;
         public string Project;
         public string State;
         public DateTime ChangedDate;
+        public string ChangedBy;
         public string Title;
         public string Collection;
         public string Uri;
@@ -19,11 +21,13 @@ namespace TFSWorkItemTracker.Models
         public PocoWorkItem(WorkItem WI, string collection)
         {
             Id = WI.Id;
+            JsonId = string.Concat(collection, WI.Id.ToString());
             Project = WI.Project.Name;
             State = WI.State;
             ChangedDate = WI.ChangedDate;
+            ChangedBy = WI.ChangedBy;
             Title = WI.Title;
-            Collection = collection;
+            Collection = collection.Split('\\')[1];
             Uri = WI.Uri.AbsoluteUri;
         }
     }
