9174c0ea914cd5bd177f5d9da4425ba1f5f3d3cd
Patrick McMorran <patrick.mcmorran@aig.com>
2015-02-05 01:10:42 -0500

    Scaffolded first iteration of reporting page.

--- TFSWorkItemTracker/Controllers/HomeController.cs
+++ TFSWorkItemTracker/Controllers/HomeController.cs
@@ -9,7 +9,10 @@ using System.IO;//Excel
 using System.Data;//Excel
 using Microsoft.TeamFoundation.Client;//tfs
 using Microsoft.TeamFoundation.WorkItemTracking.Client;//tfs
-using PagedList;
+using System.Collections.Concurrent;//concurrency
+using System.Threading.Tasks;//concurrency
+using PagedList;//pagnation
+using TFSWorkItemTracker.Models;
 
 namespace TFSWorkItemTracker.Controllers
 {
@@ -28,7 +31,7 @@ namespace TFSWorkItemTracker.Controllers
         //All if the bools represent different states
         public ActionResult Report(int? page, DateTime? StartDate, DateTime? EndDate, string currentFilter, string searchString, bool? New, bool? Approved, bool? Committed, bool? Done, bool? Removed)
         {
-            List<WorkItem> WorkItems = new List<WorkItem>();
+            ConcurrentBag<PocoWorkItem> WorkItems = new ConcurrentBag<PocoWorkItem>();
             if (searchString != null)
             {
                 page = 1;
@@ -53,48 +56,50 @@ namespace TFSWorkItemTracker.Controllers
                 QDone = true;
                 QRemoved = true;
             }
-            ///Build Connection to TFS
-            List<Uri> ProjectCollectionUris = new List<Uri>();
-            //Fetch the list of uri to the tfs server(s) project collection(s)
-            //Parses comma delimited array of Uri
-            foreach (string CollectionURI in System.Configuration.ConfigurationManager.AppSettings.Get("TFSServerUri").Split(','))
-            {
-                ProjectCollectionUris.Add(new Uri(CollectionURI.Trim()));
-            }
-            //This builds a list of project collections for querying later.
-            List<TfsTeamProjectCollection> TfsProjectCollections = new List<TfsTeamProjectCollection>();
-            foreach (Uri Location in ProjectCollectionUris)
-            {
-                TfsProjectCollections.Add(new TfsTeamProjectCollection(Location));
-            }
+
+            ViewBag.bStartDate = QStartDate;
+            ViewBag.bEndDate = QEndDate;
+            ViewBag.bNew = QNew;
+            ViewBag.bApproved = QApproved;
+            ViewBag.bCommitted = QCommitted;
+            ViewBag.bDone = QDone;
+            ViewBag.bRemoved = QRemoved;
+
             //Build Query
-            string TFSServerQuery = System.Configuration.ConfigurationManager.AppSettings.Get("TFSServerQuery"); //This needs to be changed
-            Dictionary<string, Query>  TFSServerQuerys = new Dictionary<string, Query>();
-            foreach (TfsTeamProjectCollection ProjectCollection in TfsProjectCollections)
-            {
-                TFSServerQuerys.Add(ProjectCollection.Name, new Query((WorkItemStore)ProjectCollection.GetService(typeof(WorkItemStore)), TFSServerQuery));
-            }
+            string TFSServerQuery = System.Configuration.ConfigurationManager.AppSettings.Get("TFSServerQuery2"); //This needs to be changed
+            
             int TFSServerQueryTimeout = int.Parse(System.Configuration.ConfigurationManager.AppSettings.Get("TFSServerQueryTimeout"));
-            //Run Query to get all Workitems
-            Dictionary<string, ICancelableAsyncResult> CallBacks = new Dictionary<string, ICancelableAsyncResult>();
-            //Start the Async Queries
-            foreach (string ProjectName in TFSServerQuerys.Keys)
+                    
+            Parallel.ForEach(System.Configuration.ConfigurationManager.AppSettings.Get("TFSServerUri").Split(','), UriString =>
             {
-                CallBacks.Add(ProjectName, TFSServerQuerys[ProjectName].BeginQuery());
-            }
-            Parallel.ForEach(CallBacks.Keys, ProjName =>
-            {
-                CallBacks[ProjName].AsyncWaitHandle.WaitOne(TFSServerQueryTimeout, false);
-            });
-
-
-
-
-
+                //Create Connection to Project Collection
+                TfsTeamProjectCollection ProjectCollection = new TfsTeamProjectCollection(new Uri(UriString));
+                //Build Query Object for Work Items
+                Query AsyncQuery = new Query((WorkItemStore)ProjectCollection.GetService(typeof(WorkItemStore)), TFSServerQuery);
+                //Start Async Query
+                ICancelableAsyncResult AsyncStatus = AsyncQuery.BeginQuery();
+                //Wait for query
+                AsyncStatus.AsyncWaitHandle.WaitOne(TFSServerQueryTimeout, false);
+                if (!AsyncStatus.IsCompleted)
+                {
+                    //Query hasnt returned. Cancel the query
+                    AsyncStatus.Cancel();
+                }
+                else
+                {
+                    //Query has returned, lets find any new results
+                    WorkItemCollection results = AsyncQuery.EndQuery(AsyncStatus);
+                    //Builds master list of Items to delete. For each loops do not reevaluate their index, so removing in the loop could cause issues.
+                    foreach (WorkItem WI in results)
+                    {
+                        WorkItems.Add(new PocoWorkItem(WI, WI.Project.Name));
+                    }
+                }
+            });            
 
             int pageSize = 30;
             int pageNumber = (page ?? 1);
-            return View();
+            return View(WorkItems.ToPagedList(pageNumber, pageSize));
         }
 
         //Filled with example code, will have to fill.
